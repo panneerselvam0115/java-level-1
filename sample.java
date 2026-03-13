83d95cdade5f79812f22d22b554b3a3964c749af
panneerselvam0115 <smilyboyselvam61@gmail.com>
2026-03-13 20:38:25 +0530

    ex1

---
+++ sample.java
@@ -0,0 +1,10 @@
+ import java.awt.*;
+
+class FrameDemo {
+    public static void main(String args[]) {
+        Frame f = new Frame();
+        f.setTitle("smilyselavm,-BCA");
+        f.setSize(500, 400);
+        f.setVisible(true);
+    }
+}
